bf100a1fe660f34c029832d21be83bfa41d3ae0e
apple <joy118118>
2017-06-05 19:14:22 +0800

    测试

---
+++ app/src/main/java/com/learn/apple/mystudymaterials/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.learn.apple.mystudymaterials;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by apple on 2017/6/5.
+ */
+
+public class FirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
